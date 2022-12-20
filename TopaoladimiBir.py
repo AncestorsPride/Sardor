3df88b119ce28859e73d58136648ec79de9276e1
AncestorsPride <53060818+AncestorsPride@users.noreply.github.com>
2022-12-20 16:21:45 -0500

    Create TopaoladimiBir.py
    Birinchi Sinov

---
+++ TopaoladimiBir.py
@@ -0,0 +1,13 @@
+import math, time
+
+def Topaoladimi(x, y):
+    qiyinichilik = x
+    yahshiliki = y
+    
+    if qiyinchilik > yahshilik
+       return "Nimadan qilingansa?"
+    else
+       return "Yahshi kunlar qachon tugaydi?"
+       
+print(Topaoladimi(0, 0.0000000000000001))
+    
