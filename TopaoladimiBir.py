be84c86cd6405fcae3bb349f49702f8d2c8d722c
AncestorsPride <53060818+AncestorsPride@users.noreply.github.com>
2022-12-20 16:22:49 -0500

    Update TopaoladimiBir.py

--- TopaoladimiBir.py
+++ TopaoladimiBir.py
@@ -4,9 +4,9 @@ def Topaoladimi(x, y):
     qiyinichilik = x
     yahshiliki = y
     
-    if qiyinchilik > yahshilik
+    if qiyinchilik > yahshilik:
        return "Nimadan qilingansa?"
-    else
+    else:
        return "Yahshi kunlar qachon tugaydi?"
        
 print(Topaoladimi(0, 0.0000000000000001))
